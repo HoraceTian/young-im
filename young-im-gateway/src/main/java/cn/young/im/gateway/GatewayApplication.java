e0d70ef4d9d0bf0529d3be80e85e39f55a45add8
田浩然 <1315077391@qq.com>
2023-11-12 02:22:30 +0800

    2023-11-12 沈自在 nacos

--- young-im-gateway/src/main/java/cn/young/im/gateway/GatewayApplication.java
+++ young-im-gateway/src/main/java/cn/young/im/gateway/GatewayApplication.java
@@ -1,6 +1,11 @@
 package cn.young.im.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
     }
 }
