cfe41af20235a7ce597563eb0464942e320b1d41
田浩然 <1315077391@qq.com>
2023-12-09 17:09:52 +0800

    feature 添加注册适配配置

---
+++ young-im-spring-boot-starter/young-im-spring-boot-starter-adapter/young-im-spring-boot-starter-adapter-registry/src/main/java/cn/young/im/springboot/starter/adapter/registry/AdapterRegistryConfiguration.java
@@ -0,0 +1,13 @@
+package cn.young.im.springboot.starter.adapter.registry;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 作者：沈自在 <a href="https://www.szz.tax">Blog</a>
+ *
+ * @description
+ * @date 2023/12/9
+ */
+@Configuration
+public class AdapterRegistryConfiguration {
+}
